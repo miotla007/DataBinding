e4630bdb2c095f4188b6d81c0a2848f45d43830a
miotla007 <miotla007@wp.pl>
2018-03-22 20:31:29 +0100

    first version of data binding

---
+++ script.js
@@ -0,0 +1,56 @@
+(function (root, factory) {
+    if (typeof define === "function" && define.amd) {
+        define([], factory);
+    } else if (typeof exports === "object") {
+        module.exports = factory();
+    } else {
+        root.DataBind = factory();
+    }
+}(typeof this === 'undefined' ? window : this, function () {
+    var DataBind = function(model) {
+
+      function findValueInModel(key) {
+        var splittedKey = key.split("."),
+            value = model,
+            i = 0;
+
+        while(typeof value === "object" && typeof value !== undefined && i < splittedKey.length) {
+          value = value[splittedKey[i]];
+          i++;
+        }
+
+        return value ? value : "";
+      }
+
+      var attributes = ['href', 'src', 'alt', 'title'],
+          allElements = document.body.getElementsByTagName("*");
+
+      for (var i = 0; i < allElements.length; i++) {
+        for (var j = 0; j < attributes.length; j++) {
+          if (allElements[i].hasAttribute('data-bind-' + attributes[j])) {
+            var attributeValue = allElements[i].getAttribute('data-bind-' + attributes[j]),
+                modelValue = findValueInModel(attributeValue);
+            allElements[i].setAttribute(attributes[j], modelValue);
+          }
+
+          if(allElements[i].hasAttribute('data-bind')) {
+            var attributeValue = allElements[i].getAttribute('data-bind'),
+                modelValue = findValueInModel(attributeValue);
+            allElements[i].innerHTML = modelValue;
+          }
+        }
+      }
+
+    };
+
+    return DataBind;
+}));
+
+var model = {
+  label: "Schibsted",
+  x: {
+    y: {
+      z: "some nested property to bind"
+    }
+  },
+}
